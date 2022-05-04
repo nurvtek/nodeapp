31ffc11977f611d532fd15a4bf6ef34aa7524eec
nurvtek <tmendicoa@nurvtek.com>
2022-05-03 21:36:57 -0400

    bae

--- server.js
+++ server.js
@@ -99,7 +99,6 @@ app.get('/test', function(req,res){
 app.get('/pagecount', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
-  
   if (!db) {
     initDb(function(err){});
   }
